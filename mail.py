6713627bc85bc9982d6c297f01889b0b1e5853c4
Richard Liu <me@richard4912.me>
2017-01-03 22:35:28 -0800

    Awkward, left out parentheses

--- mail.py
+++ mail.py
@@ -1,4 +1,4 @@
 import os
 
 def mail(destination, subject, message):
-	os.system('sendemail -f mailer-daemon@richard4912.me -t ' + destination + ' -u "' + subject + '" -m "' + message + '"'
+	os.system('sendemail -f mailer-daemon@richard4912.me -t ' + destination + ' -u "' + subject + '" -m "' + message + '"')
